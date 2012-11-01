bc95565d348a5010f3bb157fdca8699fca5292a8
TangYi <tang_yi_1989@qq.com>
2012-11-01 14:48:16 +0800

    There's something wrong when get cpu percent now

---
+++ openwlee/client/collector.py
@@ -0,0 +1,89 @@
+#*_* coding=utf8 *_*
+
+from openwlee import utils
+
+import time
+from datetime import datetime
+from openwlee.client.collect.libvirt_monitor import LibvirtMonitor
+
+class Collector():
+    def __new__(cls):
+        pass
+    
+    def __init__(self):
+        pass
+    
+    def collect(self):
+        return {}
+
+class InstStatCollector(Collector):
+    def __init__(self):
+        self.libvirt_monitor = LibvirtMonitor()
+        self.last_stat_time = utils.utc_now()
+        self.last_inst_stat = self.libvirt_monitor.get_all_doms_stats()
+    
+    def collect(self):
+        current_time = utils.utc_now()
+        vm_stats = self.libvirt_monitor.get_all_doms_stats()
+        inst_stats = vm_stats.copy()
+        
+        #NOTE 这个循环里面的代码看起来真的不好，谁有什么办法能让这个代码好看点
+        for inst_name in vm_stats.keys():
+            
+            inst_stat = vm_stats[inst_name]
+            inst_last_stat = self.last_inst_stat[inst_name]
+            collect_inst = inst_stats[inst_name]
+            
+            #NIC IO speed
+            for nic in inst_stat['nic_stats'].keys():
+                nic_stat = inst_stat['nic_stats'][nic]
+                last_nic_stat = inst_last_stat['nic_stats'][nic]
+                
+                collect_inst['nic_stats'][nic]['rx_bytes_speed'] = \
+                    utils.io_bytes_speed(last_nic_stat['rx_bytes'], 
+                                         nic_stat['rx_bytes'],
+                                         self.last_stat_time, current_time)
+                    
+                collect_inst['nic_stats'][nic]['tx_bytes_speed'] = \
+                    utils.io_bytes_speed(last_nic_stat['tx_bytes'], 
+                                         nic_stat['tx_bytes'],
+                                         self.last_stat_time, current_time)
+                    
+            #DISK IO speed        
+            for disk in inst_stat['disk_stats'].keys():
+                disk_stat = inst_stat['disk_stats'][disk]
+                last_disk_stat = inst_last_stat['disk_stats'][disk]
+                
+                collect_inst['disk_stats'][disk]['wr_bytes_speed'] = \
+                    utils.io_bytes_speed(last_disk_stat['wr_bytes'], 
+                                         disk_stat['wr_bytes'],
+                                         self.last_stat_time, current_time)
+                    
+                collect_inst['disk_stats'][disk]['rd_bytes_speed'] = \
+                    utils.io_bytes_speed(last_disk_stat['rd_bytes'], 
+                                         disk_stat['rd_bytes'],
+                                         self.last_stat_time, current_time)
+                    
+            #CPU percent
+            last_cputime = inst_last_stat['overview']['cputime']
+            cputime = inst_stat['overview']['cputime']
+            ncpus = inst_stat['overview']['ncpus']
+            collect_inst['overview']['cpu_percent'] = \
+                utils.cpu_percent(last_cputime, cputime, self.last_stat_time, 
+                                  current_time, ncpus)
+                    
+        self.last_inst_stat = vm_stats
+        self.last_stat_time = current_time
+        
+        return inst_stats
+        
+
+if __name__ == "__main__":
+    import pprint
+    inst_collector = InstStatCollector()
+    while True:
+        time.sleep(1)
+        stat = inst_collector.collect()['instance-000001a1']
+        utils.clear_screen()
+        pprint.pprint(stat)
+        
--- openwlee/utils.py
+++ openwlee/utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import inspect
 
 def methods_with_decorator(cls, decorator_name):
@@ -12,3 +13,25 @@ def methods_with_decorator(cls, decorator_name):
             method_names.append(name)
             
     return method_names
+
+def cpu_percent(start_cputime, end_cputime, start_time, end_time, ncpus):
+    delta_time = end_time - start_time
+    delta_cputime = (end_cputime - start_cputime) / 1000
+    if delta_cputime < 0 :
+        delta_cputime = end_cputime
+    
+    return delta_cputime * 1.0 / (delta_time.microseconds * ncpus)
+
+def io_bytes_speed(start_bytes, end_bytes, start_time, end_time):
+    delta_time = end_time - start_time
+    delta_bytes = end_bytes - start_bytes
+    if delta_bytes < 0 :
+        delta_bytes = end_bytes
+        
+    return delta_bytes * 1000000 / delta_time.microseconds
+
+def utc_now():
+    return datetime.utcnow()
+
+def clear_screen():
+    print "\033[2J"
