7d56000d0600fb0d4a4ff078be1dbba77bcac689
TangYi <tang_yi_1989@qq.com>
2012-11-09 09:39:36 +0800

    delete WebSystem

--- GetNodeInfo-CC/GNI_ConDatDef/_lib_/__init__.py
+++
@@ -1,7 +0,0 @@
-#######################################################
-#File Name: __init__.py
-#Version  : v1.0
-#Author   : Li Desheng
-#Time     : 2012-7-23
-#Usage    : Initial the modules in this folder
-#######################################################
--- WebSystem/conf/proxy_nginx2/referer.lua
+++
@@ -1,6 +0,0 @@
-local method   = ngx.var.request_method
-
-if method != 'GET' then
-	proxy_pass http://mycgiserver
-else
-	proxy_pass http://myhttpserver
