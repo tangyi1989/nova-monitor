097d093e2a7184c8245415f6d7eeba1c6cfc0fb4
TangYi <tang_yi_1989@qq.com>
2012-11-29 17:02:57 +0800

    modify wlee-agent.py

--- openwlee/wlee-agent.py
+++ openwlee/wlee-agent.py
@@ -1,36 +1,48 @@
 #*_* coding=utf8 *_*
 
 import time
+import json
 from datetime import datetime
-from openwlee.collect.collector import CollectorManager
-from openwlee.collect.instance import InstancePerfCollector
+
+from openwlee import utils
+from openwlee.common.reporter import FakeReporter, DisplayReporter
+from openwlee.monitor.monitor import MonitorManager
+from openwlee.monitor.instance_monitor import InstancePerfMonitor
 
 """
-Agent : collect data from this node, and send it to server
+Agent : collect data from this node, and report it to server
 """
 class WleeAgentManager:
-    
     def __init__(self):
-        inst_perf_collector = InstancePerfCollector()
+        inst_perf_monitor = InstancePerfMonitor()
         
-        self.collector_manager = CollectorManager()
-        self.collector_manager.append(inst_perf_collector)
+        self.hostname = utils.hostname()
+        self.reporter = DisplayReporter()
+        self.monitor_manager = MonitorManager()
+        self.monitor_manager.append(inst_perf_monitor)
     
-    def periodic_task(self):
-        data = self.collector_manager.collect_data()
-        self.display(data)
-        self.send()
+    def report_monitor_data(self):
+        data_list = self.monitor_manager.collect_data()
+        for data in data_list:
+            self.report(data)
     
-    def send(self):
-        pass
-    
-    """A method for display vm info for people see"""
-    def display(self, data):
-        import pprint
-        pprint.pprint(data)
+    def report_alive(self):
+        heartbeat = dict(type='heartbeat', data = {})
+        self.report(heartbeat)
+        
+    def report(self, data):
+        if not data.has_key('hostname'):
+            data['hostname'] = self.hostname
+            
+        if not data.has_key('timestamp'):
+            data['timestamp'] = utils.utc_now()
+            
+        report_data = utils.json_dumps(data)
+        self.reporter.report(report_data)
     
 if __name__ == "__main__":
-    client = WleeAgentManager()
+    agent = WleeAgentManager()
     while True:
         time.sleep(1)
-        client.periodic_task()
+        agent.report_monitor_data()
+        agent.report_alive()
